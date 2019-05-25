8253ef4fd8c8c6012b1c7891eef7a760782db63a
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-05-25 16:27:57 -0300

    integração com docker

--- blueprints/docker.py
+++ blueprints/docker.py
@@ -1,25 +1,55 @@
 
 import flask
+import docker
 
 blueprint = flask.Blueprint('docker', __name__)
 
+connection = docker.DockerClient()
+
 @blueprint.route('/docker', methods=[ 'GET' ])
-def get_docker():    
+def get_docker():
 
-    context = {
-        'page': 'docker',
-        'route': {
-            'is_public': False
-        },
-    }
+	container = connection.containers.get('87e17681654a')
 
-    return flask.render_template('docker.html', context=context)
+	context = {
+		'page': 'docker',
+		'container': container, 
+		'route': {
+			'is_public': False
+		},
+	}
 
-@blueprint.route('/docker/start', methods=[ 'POST' ])
+	return flask.render_template('docker.html', context=context)
+
+@blueprint.route('/docker/start', methods=[ 'GET' ])
 def start_docker():
-    return ''
 
-@blueprint.route('/docker/stop', methods=[ 'POST' ])
+	container = connection.containers.get('87e17681654a')
+
+	if not container:
+		flask.flash('Container não encontrado', 'danger')
+
+	elif container.status!= 'running':
+		container.start()
+		flask.flash('container iniciado', 'success')
+	else:
+		flask.flash('Container já está iniciado', 'info')
+	
+	return flask.redirect('/docker')
+
+@blueprint.route('/docker/stop', methods=[ 'GET' ])
 def stop_docker():
-    return ''
+
+	container = connection.containers.get('87e17681654a')
+
+	if not container:
+		flask.flash('container não encontrado', 'danger')
+
+	elif container.status == 'running':
+		container.stop()
+		flask.flash('container parado', 'success')
+	else:
+		flask.flash('Container já está parado', 'info')
+	
+	return flask.redirect('/docker')
 
